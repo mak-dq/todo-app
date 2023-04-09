9313f89ff5ab428b79a784ccfb1b6b8ead88face
Mohammed Amaan Khan <amaanrkhan9@gmail.com>
2023-04-09 20:27:03 +0530

    feat: add addTask() function and its styles

---
+++ script.js
@@ -0,0 +1,40 @@
+let input = document.getElementById("text-input");
+let tasks = document.getElementById("tasks");
+
+const markComplete = (x) => {
+  console.log("x :>> ", x);
+};
+
+const removeTask = (x) => {
+  console.log("x :>> ", x);
+};
+
+const addTask = () => {
+  if (input.value.trim() === "") {
+    alert("Task can't be empty");
+    return;
+  }
+  let taskId = Date.now();
+  let currTasks = Array.from(JSON.parse(localStorage.getItem("tasks")) ?? []);
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify([
+      ...currTasks,
+      { task: input.value.trim(), id: taskId, completed: false },
+    ])
+  );
+
+  let li = document.createElement("li");
+  li.innerHTML = `
+    <div class="check-text">
+        <input type="checkbox" onclick="markComplete(${taskId})">
+        <p>${input.value}</p>
+    </div>
+    <button onclick="removeTask(${taskId})">x</button>
+    `;
+
+  input.value = "";
+  input.focus();
+
+  tasks.appendChild(li);
+};
